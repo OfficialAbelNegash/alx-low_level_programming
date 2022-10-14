a0ddc6d2e224778290841bc21a11e0e56407de27
Abel Negash <abelnegash2015@gmail.com>
2022-10-13 17:25:46 -0700

    task 8 completed

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -5,11 +5,12 @@
  *main - Entry point
  *
  *Description: print a quote using write function
- *	ssize_t write(int fd, const void *buf, size_t count);
+ *
+ *    ssize_t write(int fd, const void *buf, size_t count);
  *
  *Return: This time we return an error 1.
  *
- */
+*/
 
 int main(void)
 
