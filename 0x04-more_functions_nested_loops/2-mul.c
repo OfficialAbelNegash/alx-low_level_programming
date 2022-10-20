922e9a690422a77408f42a91f0c555d65fb785cd
Abel Negash <abelnegash2015@gmail.com>
2022-10-21 00:34:27 +0300

    task 02 commited

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * mul - Entry point
+ * @a: First operand
+ * @b: Second operand
+ *
+ * Description: Add question here
+ * Return: a multiplied by b
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
+
